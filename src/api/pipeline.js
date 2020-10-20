d6903074b8267b25f5f34102628d50f2a14f3ad7
Jia Jia <angersax@sina.com>
2020-10-20 21:07:38 +0800

    Update test data for pipeline

--- src/api/pipeline.js
+++ src/api/pipeline.js
@@ -7,47 +7,89 @@ const Pipelines = [
     time: '2020-10-20 00:00:00'
   },
   {
-    name: 'Go 2020-10-20',
-    status: 'pass',
-    stage: 'test',
+    name: 'C# 2020-10-20',
+    status: 'running',
+    stage: 'build',
     owner: 'name@example.com',
     time: '2020-10-20 00:00:01'
   },
   {
-    name: 'Java 2020-10-20',
-    status: 'fail',
-    stage: 'deploy',
+    name: 'Dart 2020-10-20',
+    status: 'running',
+    stage: 'build',
     owner: 'name@example.com',
     time: '2020-10-20 00:00:02'
   },
   {
-    name: 'Node.js 2020-10-20',
-    status: 'running',
-    stage: 'build',
+    name: 'Go 2020-10-20',
+    status: 'pass',
+    stage: 'test',
     owner: 'name@example.com',
     time: '2020-10-20 00:00:03'
   },
   {
-    name: 'PHP 2020-10-20',
+    name: 'Java 2020-10-20',
     status: 'pass',
     stage: 'test',
     owner: 'name@example.com',
     time: '2020-10-20 00:00:04'
   },
   {
-    name: 'Python 2020-10-20',
-    status: 'fail',
-    stage: 'deploy',
+    name: 'JavaScript 2020-10-20',
+    status: 'pass',
+    stage: 'test',
     owner: 'name@example.com',
     time: '2020-10-20 00:00:05'
   },
   {
-    name: '.NET Core 2020-10-20',
+    name: 'Kotlin 2020-10-20',
+    status: 'fail',
+    stage: 'deploy',
+    owner: 'name@example.com',
+    time: '2020-10-20 00:00:06'
+  },
+  {
+    name: 'PHP 2020-10-20',
+    status: 'fail',
+    stage: 'deploy',
+    owner: 'name@example.com',
+    time: '2020-10-20 00:00:07'
+  },
+  {
+    name: 'Python 2020-10-20',
+    status: 'fail',
+    stage: 'deploy',
+    owner: 'name@example.com',
+    time: '2020-10-20 00:00:08'
+  },
+  {
+    name: 'Ruby 2020-10-20',
     status: 'running',
     stage: 'build',
     owner: 'name@example.com',
-    time: '2020-10-20 00:00:06'
-  }
+    time: '2020-10-20 00:00:09'
+  },
+  {
+    name: 'Rust 2020-10-20',
+    status: 'running',
+    stage: 'build',
+    owner: 'name@example.com',
+    time: '2020-10-20 00:00:10'
+  },
+  {
+    name: 'Swift 2020-10-20',
+    status: 'running',
+    stage: 'build',
+    owner: 'name@example.com',
+    time: '2020-10-20 00:00:11'
+  },
+  {
+    name: 'TypeScript 2020-10-20',
+    status: 'running',
+    stage: 'build',
+    owner: 'name@example.com',
+    time: '2020-10-20 00:00:12'
+  },
 ]
 
 const getPipeline = limit => {
